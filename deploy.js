f8592db391cbfba824ecb484a21e15f4a30b79b3
Vincent <vincent.osele@spherea.com>
2017-12-08 15:18:29 +0100

    photos agrandies

--- deploy.js
+++ deploy.js
@@ -2,6 +2,7 @@ define([
     'amber/deploy',
     'bootstrap/js/bootstrap',
     'css!bower_components/font-awesome/css/font-awesome',
+    'css!../css/jct-photos',
     // --- packages to be deployed begin here ---
     'amber-jctphotos/JCTPhotos'
     // --- packages to be deployed end here ---
--- devel.js
+++ devel.js
@@ -3,9 +3,8 @@ define([
     'amber/devel',
     'bootstrap/js/bootstrap',
     'css!bower_components/font-awesome/css/font-awesome',
+    'css!../css/jct-photos',
     // --- packages used only during development begin here ---
-    'amber/legacy/Benchfib',
-    'amber/legacy/Examples',
     'amber/legacy/IDE'
     // --- packages used only during development end here ---
 ], function (amber) {
